7c3170ccbb61524f6aea0dc62c77ca333408d65c
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2015-10-04 21:32:25 +0200

    Implement game loop into GameCanvas and install ESLint for babel

--- src/models/canvas.js
+++ src/models/canvas.js
@@ -1,12 +1,9 @@
 'use strict';
 
 export default class GameCanvas {
-  constructor(domNode, top = 0, left = 0, height = 0, width = 0) {
+  constructor(domNode) {
     this.domNode = domNode;
-    this.top = top;
-    this.left = left;
-    this.height = height;
-    this.width = width;
+    this.ctx = domNode.getContext('2d');
 
     // when passing this, we can implement a method named `handleEvent`,
     // that handles it
@@ -14,8 +11,17 @@ export default class GameCanvas {
 
     // initially, trigger the event once canvas is initialized
     window.dispatchEvent(new Event('resize'));
+
+    // game loop
+    window.now = null;
+    window.dt = null;
+    this.last = this.timestamp();
+
+    // start render loop
+    requestAnimationFrame(this.frame.bind(this));
   }
 
+  // handles the window object's resize event
   handleEvent({ target: { innerHeight, innerWidth }}) {
     this.domNode.style.padding = '0 0 0 0';
     this.domNode.style.margin = '0 0 0 0';
@@ -23,4 +29,25 @@ export default class GameCanvas {
     this.domNode.style.height = innerHeight + 'px';
     this.domNode.style.width = innerWidth + 'px';
   }
-};
+
+  timestamp() {
+    return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
+  }
+
+  frame() {
+    this.now = this.timestamp();
+    this.dt = (this.now - this.last) / 1000;
+    this.update(this.dt);
+    this.render(this.dt);
+    this.last = this.now;
+    requestAnimationFrame(this.frame.bind(this));
+  }
+
+  update() {
+
+  }
+
+  render() {
+
+  }
+}
